ae60dfcd2edf0efac5734b63fbb363298614cd41
bryanrtboy <bryan@bryanleister.com>
2019-12-12 15:01:55 -0700

    Added Json datasaving

--- Assets/OpenWeather/Scripts/GetWeatherInfo.cs
+++ Assets/OpenWeather/Scripts/GetWeatherInfo.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 using UnityEngine.Networking;
 using System;
 using UnityEngine.UI;
+using System.IO;
 
 public class GetWeatherInfo : MonoBehaviour
 {
@@ -16,6 +17,8 @@ public class GetWeatherInfo : MonoBehaviour
     public float m_lat = 39.7661f;
     [Tooltip("If you are not building to a mobile device, use a placeholder longitude")]
     public float m_lon = -105.077209f;
+    [Tooltip("If you are not building to a mobile device, use a placeholder longitude")]
+    public TextAsset[] m_backupReports;
     [Tooltip("View some basic conditions in the game view here:")]
     public Text m_UI;
 
@@ -23,13 +26,16 @@ public class GetWeatherInfo : MonoBehaviour
 
 
     string m_url = "";
+    int count = 0;
 
     void OnEnable()
     {
         m_url = m_openMapAPIRootURL + "lat=" + m_lat.ToString() + "&lon=" + m_lon.ToString() + "&appid=" + m_openMapAPIKey;
-
         StartCoroutine(GetRequest(m_url));
 
+        //Keep track of number of times we have saved weather data
+        if (PlayerPrefs.HasKey("Count"))
+            count = PlayerPrefs.GetInt("Count");
     }
 
     IEnumerator GetRequest(string uri)
@@ -45,14 +51,55 @@ public class GetWeatherInfo : MonoBehaviour
             if (webRequest.isNetworkError)
             {
                 Debug.Log(pages[page] + ": Error: " + webRequest.error);
+
+                //If there is no internet, use a backup session
+                Load(m_backupReports[UnityEngine.Random.Range(0, m_backupReports.Length)].text);
             }
             else
             {
                 Debug.Log(pages[page] + ":\nReceived: " + webRequest.downloadHandler.text);
+                SaveData(webRequest.downloadHandler.text);
                 Load(webRequest.downloadHandler.text);
 
             }
         }
+    }
+
+    void SaveData(string savedData)
+    {
+        string dateValue = System.DateTime.Now.Day.ToString() + System.DateTime.Now.Month.ToString() + System.DateTime.Now.Year.ToString();
+        string dateKey = "Date" + count.ToString(); ;
+
+        if (PlayerPrefs.HasKey(dateKey))
+        {
+            if (PlayerPrefs.GetString(dateKey) == dateValue)
+            {
+                Debug.Log("Record already fetched for today, not saving again");
+                return;
+            }
+            else
+            {
+                PlayerPrefs.SetString(dateKey, dateValue);
+                WriteCurrentDataToFile(savedData);
+            }
+        }
+        else
+        {
+            PlayerPrefs.SetString(dateKey, dateValue);
+            WriteCurrentDataToFile(savedData);
+        }
+    }
+
+    void WriteCurrentDataToFile(string savedData)
+    {
+        count++;
+        if (count > 10)
+            count = 0;
+
+        PlayerPrefs.SetInt("Count", count);
+        StreamWriter writer = new StreamWriter("Assets/OpenWeather/Data/" + count.ToString() + ".json", false);
+        writer.Write(savedData);
+        writer.Close();
     }
 
     public void Load(string savedData)
@@ -62,8 +109,10 @@ public class GetWeatherInfo : MonoBehaviour
         DateTime dateTime = new DateTime();
         dateTime = dateTime.AddSeconds(m_currentConditions.dt);
 
+        float temp = m_currentConditions.main.temp * 9 / 5 - 459.67f;
+
         if (m_UI != null)
-            m_UI.text = "The weather is " + m_currentConditions.weather[0].main + " in " + m_currentConditions.name;
+            m_UI.text = dateTime.ToShortDateString() + ": It's " + temp.ToString("#.#") + " degrees F and " + m_currentConditions.weather[0].main + " in " + m_currentConditions.name;
     }
 
 }
